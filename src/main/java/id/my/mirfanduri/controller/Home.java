8284a78861088d9e2a82b7ad476ce59044a38ee1
sidulanaksekolahan <mirfan.duri@gmail.com>
2024-07-06 17:23:29 +0700

    Adding new endpoint

--- src/main/java/id/my/mirfanduri/controller/Home.java
+++ src/main/java/id/my/mirfanduri/controller/Home.java
@@ -3,6 +3,7 @@ package id.my.mirfanduri.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,11 @@ public class Home {
     @GetMapping
     public ResponseEntity<String> home(){
         return new ResponseEntity<>("Hello Muhammad Irfan, you are using Jenkins!", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{name}")
+    public ResponseEntity<String> printMyName(@PathVariable String name) {
+        return new ResponseEntity<>("Hai " + name +
+                ", you are using CI/CD with Jenkins. Congratulations!", HttpStatus.OK);
     }
 }
