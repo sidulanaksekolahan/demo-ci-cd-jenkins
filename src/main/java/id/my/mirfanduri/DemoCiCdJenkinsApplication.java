4020a336ccce68712aa7d9c8d9e20baac34a194f
sidulanaksekolahan <mirfan.duri@gmail.com>
2024-07-06 15:32:04 +0700

    First commit

---
+++ src/main/java/id/my/mirfanduri/DemoCiCdJenkinsApplication.java
@@ -0,0 +1,13 @@
+package id.my.mirfanduri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCiCdJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCiCdJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/my/mirfanduri/DemoCiCdJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.mirfanduri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiCdJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
