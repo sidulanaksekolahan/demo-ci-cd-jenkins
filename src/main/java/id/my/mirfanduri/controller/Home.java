69eb3d3b9d462e2da060bf9eaac611351f853f5c
sidulanaksekolahan <mirfan.duri@gmail.com>
2024-07-08 18:02:12 +0700

    Update

--- src/main/java/id/my/mirfanduri/controller/Home.java
+++ src/main/java/id/my/mirfanduri/controller/Home.java
@@ -1,5 +1,6 @@
 package id.my.mirfanduri.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/home")
 public class Home {
 
     @GetMapping
     public ResponseEntity<String> home(){
+        log.info(">>> request to home");
         return new ResponseEntity<>("Hello Muhammad Irfan, you are using Jenkins!. Your dockerhub is mirfanduri",
                 HttpStatus.OK);
     }
 
     @GetMapping(path = "/{name}")
     public ResponseEntity<String> printMyName(@PathVariable String name) {
+        log.info(">>> request to printMyName");
         return new ResponseEntity<>("Hai " + name +
                 ", you are using CI/CD with GitHub and Jenkins. Congratulations!", HttpStatus.OK);
     }
